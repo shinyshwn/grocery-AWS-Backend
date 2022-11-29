b10d8482feefce3fa1bb2cdc19cd85b53004390f
Shenghao Guo <sguo5@wpi.edu>
2022-11-29 04:21:48 +0000

    corp generate inventory

---
+++ stores/corp_generate_inventory/app.js
@@ -0,0 +1,164 @@
+// const axios = require('axios')
+// const url = 'http://checkip.amazonaws.com/';
+let response;
+const mysql = require('mysql');
+
+var config = require('./config.json');
+//create a mysql pool 
+var pool = mysql.createPool({
+    
+    host: config.host,
+    user: config.user,
+    password: config.password,
+    database: config.database
+});
+
+// https://www.freecodecamp.org/news/javascript-promise-tutorial-how-to-resolve-or-reject-promises-in-js/#:~:text=Here%20is%20an%20example%20of,message%20Something%20is%20not%20right!%20.
+function query(conx, sql, params) {
+    return new Promise((resolve, reject) => {
+         conx.query(sql, params, function(err, rows) {
+            if (err) {
+                // reject because there was an error
+                reject(err);
+            } else {
+                // resolve because we have result(s) from the query. it may be an empty rowset or contain multiple values
+                resolve(rows);
+            }
+        });
+    });
+}
+
+
+// Take in as input a payload.
+//
+// {
+//   "body" : "{ \"sku\" : \"ZH7Sf35E\", \"shelf\" : \"5\" , \"aisle\" : \"100\"}"
+// }
+//
+// ===>  { "Assigned location successfully" }
+//
+//
+
+exports.lambdaHandler = async (event, context, callback) => {
+    context.callbackWaitsForEmptyEventLoop = false;
+
+   // ready to go for CORS. To make this a completed HTTP response, you only need to add a statusCode and a body.
+    let response = {
+        headers: {
+            "Access-Control-Allow-Headers": "Content-Type",
+            "Access-Control-Allow-Origin": "*", // Allow from anywhere
+            "Access-Control-Allow-Methods": "POST" // Allow POST request
+        }
+    }; // response
+
+    var inventory_result = [];
+    var item_prices = [];
+    var store_total_value = 0; 
+
+    // get raw value or, if a string, then get from database if exists.
+    let getInventory = () => {
+       
+            return new Promise((resolve, reject) => {
+                pool.query("SELECT store_id, sku, quantity FROM inventory ORDER BY store_id;" , (error, rows) => {
+                                if (error) {
+                                    console.log("error"); 
+                                    // return reject(error); 
+                                    return reject(error)
+                                }
+                                if(rows){
+                                    if(rows.length < 1){
+                                        return reject(" No inventory for the store ")
+                                    }
+                                    inventory_result = rows; 
+                                    //console.log(JSON.stringify(inventory_result));
+                                    //console.log(rows.length);
+                                    return resolve(true)
+                                }
+                                else{
+                                    return reject("unable to get inventory from the table " );
+                                }
+                            })
+            });
+        
+    }
+    
+    let getPrice = () => {
+
+            return new Promise((resolve, reject) => {
+                pool.query("SELECT sku, price FROM items WHERE sku IN (SELECT sku FROM inventory ORDER BY sku);", (error, rows) => {
+                                if (error) {
+                                    console.log("error"); 
+                                    // return reject(error); 
+                                    return reject(error)
+                                }
+                                if(rows){
+                                    item_prices = rows; 
+                                    //console.log(JSON.stringify(item_prices));
+                                    return resolve(true)
+                                }
+                                else{
+                                    return reject("unable to get prices from the table ");
+                                }
+                            })
+            });
+    }
+    
+
+    try {
+        
+        // 1. Query RDS for the first constant value
+        // 2. Query RDS for the second constant value
+        // ----> These have to be done asynchronously in series, and you wait for earlier 
+        // ----> request to complete before beginning the next one
+        let getInven = await getInventory();
+        let getP = await getPrice();
+
+        
+        // If either is NaN then there is an error
+        if (getInven == true) {
+            //console.log(JSON.stringify(inventory_result)); 
+            //console.log(JSON.stringify(item_prices)); 
+            for (const entry of inventory_result) {
+               for (const item of item_prices) { 
+                   if(entry.sku == item.sku){
+                       entry.price = item.price; 
+                       //console.log(entry);
+                   }
+               }
+                
+            }
+            //console.log("after: " + JSON.stringify(inventory_result)); 
+            
+            //console.log(arr3); 
+            inventory_result.forEach((entry) =>{
+                
+                entry.item_total_value = entry.price * entry.quantity;
+                store_total_value = store_total_value + entry.item_total_value;
+                //console.log(entry);
+            
+            });
+            //console.log(store_total_value);
+            let store_inventroy_total_value = "store_total_inventroy_value: " + store_total_value; 
+            inventory_result.push(store_inventroy_total_value); 
+            //console.log(arr3);
+            response.statusCode = 200;
+            // response.result = JSON.stringify(arg1_value);
+           
+            response.result = inventory_result;
+            
+        } else {
+            // otherwise SUCCESS!
+            console.log("error: " + getInven.reject);
+            response.statusCode = 400;
+            response.error = JSON.stringify(getInven);
+        }
+    } catch (error) {
+        console.log("ERROR: " + error);
+        response.statusCode = 400;
+        response.error = error;
+    }
+    
+    // full response is the final thing to send back
+    return response;
+}
+
